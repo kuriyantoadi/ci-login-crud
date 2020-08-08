61a282a90587816d973a951d1eb5cba9e707e328
kuriyantoadi <melatikuning212@gmail.com>
2020-08-08 20:52:34 +0700

    update 8 agustus 2020, update 2

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# ci-login-crud
-ci login crud
+# CodeIgniter-Bootstrap-CRUD
+
+**1. Extrak Framework CI**
+
+**2. Import database db-buku.sql**
+
+**3. application/config/autoload.php**
+  <br>Awal Baris 92 :
+  <br>`$autoload['helper'] = array('');`
+  <br>Edit :
+  <br>`$autoload['helper'] = array('url','form');`
+
+  <br>Awal Baris 61 :
+  <br>`  $autoload['libraries'] = array('');`
+  <br>Edit :
+  <br>`  $autoload['libraries'] = array('database');`
+
+  **4. application/config/database.php ubah baris 78-81**
+  <br>`'hostname' => 'localhost',`
+  <br>`'username' => 'root',`
+  <br>`'password' => '',`
+  <br>`'database' => 'db_buku',`
+
+  **5. application/config/config.php ubdah baris 26**
+  <br>`$config['base_url'] = '';`
+  <br>Edit
+  <br>Alamat disesuaikan dengan nama project
+  <br>`$config['base_url'] = 'http://localhost/ci-login-crud/';`
+
+  **6. Tambah folder asset**
+  <br>Folder ini berisi file bootstrap dan js
+
+  **7. Tambahkan/Edit beberapa file berikut**
+  - application/controllers/C_buku.php -> Controller untuk tambah, tampil, edit dan hapus buku
+  - application/controllers/C_login.php -> Controller untuk login user
+  - application/controllers/C_page.php -> Controller untuk mengarahkan ke View Tampil buku
+  - application/models/M_buku.php -> Menghubukan controller dengan Database Data Buku
+  - application/models/M_login.php -> Menghubukan controller dengan Database Login User
+  - application/view/buku/v_edit.php -> File untuk edit data buku
+  - application/view/buku/v_tambah.php -> File untuk tambah data buku
+  - application/view/buku/v_tampil.php -> File untuk tampil data buku
+
+
+  **8. application/config/routes.php ubah baris 52**
+  <br>`$route['default_controller'] = 'C_buku';`
+
+  **9. Buka dengan Link berikut**
+  <br>`localhost/ci-bootstrap-crud`
+
+  (catatan ini adalah lanjutan dari repository ci-crud-tambah)
---
+++ db_buku.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 08, 2020 at 03:44 
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_buku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_buku`
+--
+
+CREATE TABLE `tb_buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `pengarang` varchar(30) NOT NULL,
+  `tahun_terbit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`id_buku`, `judul`, `pengarang`, `tahun_terbit`) VALUES
+(1, 'buku jo1', 'diki', 2020),
+(2, '111111', 'q', 109),
+(4, 'buku mantab', 'php', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_login`
+--
+
+CREATE TABLE `tb_login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_login`
+--
+
+INSERT INTO `tb_login` (`id`, `username`, `password`, `level`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `tb_login`
+--
+ALTER TABLE `tb_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tb_login`
+--
+ALTER TABLE `tb_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
